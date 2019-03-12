c0343807999fc1500805de358e8aa20c73284075
Victor Bala <savicsly@gmail.com>
2019-03-12 11:34:48 +0100

    add code coverage
    Signed-off-by: Victor Bala <savicsly@gmail.com>

---
+++ src/layouts/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+const Layout = () => {
+    return (
+        <React.Fragment>
+            <Header />
+            <Main />
+            <Footer />
+        </React.Fragment>
+    );
+
+}
+
+export default Layout;
